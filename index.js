b86a8ed96d0664a8f17678b6a4bf0ad8b1ff3ed8
Curtis Elsasser <curt.elsasser@gmail.com>
2018-10-31 13:48:54 -0400

    include the obligatory `main` property in package.json

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * User: curtis
+ * Date: 10/31/18
+ * Time: 1:43 PM
+ * Copyright @2018 by Xraymen Inc.
+ */
+
+console.warn(`${module.filename} should not be imported. We are built for code quality only`);
